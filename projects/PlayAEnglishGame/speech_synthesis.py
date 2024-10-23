dbcd231e17f61072dd96cfc588d8dfd824d482fc
lichong6 <lichong6@kingsoft.com>
2024-10-23 17:33:33 +0800

    test text to speech

---
+++ projects/PlayAEnglishGame/speech_synthesis.py
@@ -0,0 +1,32 @@
+import os
+import azure.cognitiveservices.speech as speechsdk
+
+# This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+speech_config = speechsdk.SpeechConfig(subscription="e82807440b154003abb0db805cb70f3b", region="westus")
+audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+
+# The neural multilingual voice can speak different languages based on the input text.
+speech_config.speech_synthesis_voice_name='en-US-AvaMultilingualNeural'
+
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+
+# Get text from the console and synthesize to the default speaker.
+print("Enter some text that you want to speak >")
+text = input()
+
+speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+
+# 尝试保存声音
+with open("output.wav", "wb") as file:
+    file.write(speech_synthesis_result.audio_data)
+
+
+if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized for text [{}]".format(text))
+elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = speech_synthesis_result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
